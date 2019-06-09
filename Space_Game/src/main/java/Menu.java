30a9819e8df90032ea0526b579a65569ba907fc9
Josue Rosales <jrosa1756@gmail.com>
2019-06-08 22:34:05 -0500

    Add files via upload
    Custom print text system using a spritesheet
    Menu Design is some what there

--- Space_Game/src/main/java/Menu.java
+++ Space_Game/src/main/java/Menu.java
@@ -1,21 +1,18 @@
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
  * Space_Game
  * Created: Jun 7, 2019
- * TODO: Create Menu Design
+ * TODO: Get button to work
  */
 
 /**
  * @author coffeemate
- *
  */
 public class Menu extends JPanel{
     int xSize, ySize;
@@ -30,9 +27,15 @@ public class Menu extends JPanel{
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
+        Sprites sprites = new Sprites();
+        sprites.printText("Space", 150, ySize/10, g2d);
+        sprites.printText("Game", 180, ySize/4, g2d);
 
         g2d.setColor(Color.WHITE);
         g2d.fillRect(xSize/2 - 75, ySize/2 - 25, 150, 50);
+
+        g2d.setColor(Color.BLACK);
+        g2d.drawString("Play!", 300, 240);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     }
 
